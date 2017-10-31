7b1cd793ef1b7b43c4649bc84a440b289ea9beed
bot9527 <yunbo@uw.edu>
2017-10-30 19:58:05 -0700

    simpleCal-ios 2.1
    changed scrollview to textview

--- SimpleCalc-iOS/HistoryViewController.swift
+++ SimpleCalc-iOS/HistoryViewController.swift
@@ -12,6 +12,7 @@ private let reuseIdentifier = "Cell"
 
 class HistoryViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var textView: UITextView!
     var history:[String] = []
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -21,12 +22,14 @@ class HistoryViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if history.count > 1 {
+        if history.count > 0 {
             for i in 0...history.count-1 {
-                let label = UILabel(frame: CGRect(x: 20, y: CGFloat(i * 20), width: scrollView.frame.size.width, height: 20))
+                /*let label = UILabel(frame: CGRect(x: 20, y: CGFloat(i * 50), width: scrollView.frame.size.width, height: 50))
                 label.text = history[i]
-                label.adjustsFontSizeToFitWidth = true
-                scrollView.addSubview(label)
+                label.font = label.font.withSize(20)
+                
+                scrollView.addSubview(label)*/
+                textView.text! += history[i] + "\n" + "\n"
             }
         }
         
